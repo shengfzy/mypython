90085706b25ded31de673f1dfe4cf480db27c2f6
shengfzy <shengfzysk12345@163.com>
2019-02-19 09:47:13 +0800

    compatibility fix for network2

--- Neural networks/neural-networks-and-deep-learning-master/src/main.py
+++ Neural networks/neural-networks-and-deep-learning-master/src/main.py
@@ -1,12 +1,18 @@
 import mnist_loader
 import os
 
-os.chdir(r'C:\Users\沈恺\OneDrive\mypython\Neural networks\neural-networks-and-deep-learning-master\src')
+os.chdir(r'D:\Git\mypython\Neural networks\neural-networks-and-deep-learning-master\src')
 
 training_data, validation_data, test_data = \
 mnist_loader.load_data_wrapper()
 
-import network
+import network,network2
 
+'''
 net = network.Network([784,30,10])
 net.SGD(list(training_data), 30, 10, 3.0, test_data=list(test_data))
+'''
+
+net = network2.Network([784,30,10], cost = network2.CrossEntropyCost)
+net.large_weight_initializer()
+net.SGD(list(training_data),30,10,0.5,evaluation_data = list(test_data),monitor_evaluation_accuracy = True)
